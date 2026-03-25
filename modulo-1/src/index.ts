e42c9397f86dde9313e68139a341f61738152032
Oskar <aplotec.oj@gmail.com>
2026-03-25 14:41:17 +0100

    lab 1

--- modulo-1/src/index.ts
+++ modulo-1/src/index.ts
@@ -1,3 +1,13 @@
-import mediaAritmetica from "./math-utils.js";
+import { calcMediaAritmetica } from "./math-utils.js";
+import { calcMediana } from "./math-utils.js";
+import { filtrarValoresAtipicos } from "./math-utils.js";
 
-console.log(mediaAritmetica([1, 2, 3, 4, 5]));
+
+const valores = [10, 12, 23, 23, 16, 23, 21, 16, 100];
+
+console.log("valores: " + valores);
+console.log("media:" + calcMediaAritmetica(valores));
+console.log("mediana: " + calcMediana(valores));
+console.log("valores atipicos inferiores: " + filtrarValoresAtipicos(valores, 1.5).inferiores);
+console.log("valores atipicos superiores: " + filtrarValoresAtipicos(valores, 1.5).superiores);
+
--- modulo-1/src/math-utils.ts
+++ modulo-1/src/math-utils.ts
@@ -1,13 +1,69 @@
 // Media Aritmetica
-
-export default function mediaAritmetica(array: number[]): number | null {
-   if (array.length === 0 || array === null) {
+export function calcMediaAritmetica(array: number[]): number | null {
+   if (array.length === 0) {
     return null;
    }
-
    let i: number  = 0;
    array.forEach(num => i += num);
    return i / array.length;
 }
 
-//console.log(mediaAritmetica([1, 2, 3, 4, 5]));
+// Mediana
+export function calcMediana(array: number[]): number | null {
+    if (array.length === 0) {
+        return null;
+    }
+     // Crear copia ordenada 
+  const ordenados = [...array].sort((a, b) => a - b);
+  const mitad = Math.floor(ordenados.length / 2);
+  
+  if (ordenados.length % 2 === 0) {
+    // Par: promedio de los dos centrales
+    return ((ordenados[mitad - 1] ?? 0) + (ordenados[mitad] ?? 0)) / 2;
+  } else {
+    // Impar: valor central
+    return ordenados[mitad] ?? 0;
+  }
+}
+
+// filtrar valores atipicos
+
+export function filtrarValoresAtipicos(
+    array: number[],
+    limite: number = 1.5
+  ): { inferiores: number[]; superiores: number[] } {
+    if (array.length === 0) {
+      return { inferiores: [], superiores: [] };
+    }
+  
+    const ordenados = [...array].sort((a, b) => a - b);
+  
+    const mitad = Math.floor(ordenados.length / 2);
+  
+    const mitadInferior = ordenados.slice(0, mitad);
+    const mitadSuperior =
+      ordenados.length % 2 === 0
+        ? ordenados.slice(mitad)
+        : ordenados.slice(mitad + 1);
+  
+    const quartil1 = calcMediana(mitadInferior) ?? 0;
+    const quartil3 = calcMediana(mitadSuperior) ?? 0;
+  
+    const iqr = quartil3 - quartil1;
+  
+    const limiteInferior = quartil1 - limite * iqr;
+    const limiteSuperior = quartil3 + limite * iqr;
+  
+    const inferiores: number[] = [];
+    const superiores: number[] = [];
+  
+    for (const num of array) {
+      if (num < limiteInferior) {
+        inferiores.push(num);
+      } else if (num > limiteSuperior) {
+        superiores.push(num);
+      }
+    }
+
+    return { inferiores, superiores };
+  }
